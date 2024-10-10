11ba85b8e840e6204d6deeb2b720f0da774d8c24
AbhishekINT153 <abhishek.verma.6561@gmail.com>
2024-10-10 12:34:24 +0530

    done

--- 6node-architecture/app.js
+++ 6node-architecture/app.js
@@ -1,24 +1,26 @@
 const fs = require('fs');
-
+const os = require("os");
 // Sync...
-console.log("Sync...")
-console.log(1);
+// console.log("Sync...")
+// console.log(1);
 const resonse = fs.readFileSync("./file.txt","utf-8");
-console.log(resonse);
-console.log(2);
+// console.log(resonse);
+// console.log(2);
 
 
 //Async...
-console.log("Async...")
-console.log(1);
+// console.log("Async...")
+// console.log(1);
 const response = fs.readFile("./file.txt","utf-8",(err,res)=>{
     if(err){
         console.log("error: ", err);
     }
     else{
-        console.log(res);
+        // console.log(res);
     }
 });
-console.log("2");
-console.log("3");
-console.log("4");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+
+console.log(os.cpus().length); // TOTAL 8 CPUS so we can go for creation of 8 thread at max
